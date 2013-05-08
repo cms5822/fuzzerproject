12cd631ed293ad375808f227ac12cf8922613632
Adam <aji9861@rit.edu>
2013-05-08 19:40:48 -0400

    Change to use 127.0.0.1

--- src/example/fuzzer/JpetStoreProperties.java
+++ src/example/fuzzer/JpetStoreProperties.java
@@ -3,10 +3,10 @@ package example.fuzzer;
 public class JpetStoreProperties extends PageProperties {	
 	public JpetStoreProperties(){
 		super(
-				"http://129.21.12.221:8080",
-				"http://129.21.12.221:8080/jpetstore/",
-				"http://129.21.12.221:8080/jpetstore/actions/Account.action?newAccountForm=",
-				"http://129.21.12.221:8080/jpetstore/actions/Account.action",
+				"http://127.0.0.1:8080",
+				"http://127.0.0.1:8080/jpetstore/",
+				"http://127.0.0.1:8080/jpetstore/actions/Account.action?newAccountForm=",
+				"http://127.0.0.1:8080/jpetstore/actions/Account.action",
 				
 				"j2ee",
 				"j2ee",
